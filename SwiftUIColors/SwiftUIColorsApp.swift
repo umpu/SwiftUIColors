f6cc19359cd274f3f1b20cbdcdb45d4e31c1923e
umpu <carol.ledgers0h@icloud.com>
2024-05-02 10:24:17 +0500

    Initial Commit

---
+++ SwiftUIColors/SwiftUIColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorsApp.swift
+//  SwiftUIColors
+//
+//  Created by Sonoma on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
