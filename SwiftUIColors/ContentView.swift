ed30d66d9bbd9933d715e5f3f2d7241d4d48edce
umpu <carol.ledgers0h@icloud.com>
2024-05-02 11:49:53 +0500

    Visual representaion of all standard colors with combination of two color schemes and two contrast modes

--- SwiftUIColors/ContentView.swift
+++ SwiftUIColors/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    var colorSchemes: [ColorScheme] = [.dark, .light]
+    var colorSchemeContrasts: [ColorSchemeContrast] = [.standard, .increased]
+    var colors: [Color] = [.red, .orange, .yellow, .green, .mint, .teal, .cyan, .blue, .indigo, .purple, .pink, .brown]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(spacing: 0) {
+            ForEach(colorSchemeContrasts, id: \.self) { colorSchemeContrast in
+                ForEach(colorSchemes, id: \.self) { colorScheme in
+                    VStack(spacing: 0) {
+                        Text(colorScheme == .light ? "Light" : "Dark")
+                        Text(colorSchemeContrast == .standard ? "Standard" : "Increased")
+                        
+                        ForEach(colors, id: \.self) { color in
+                            Rectangle()
+                                .environment(\.colorScheme, colorScheme)
+                                .environment(\._colorSchemeContrast, colorSchemeContrast)
+                                .foregroundStyle(color)
+                                .overlay { Text(color.description) }
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
